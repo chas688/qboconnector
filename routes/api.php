c6a2509606725cc29e90c65a0b03f2205c4f092d
Charles Harmon <charlesharmon@Charless-MacBook-Pro.local>
2024-10-13 10:04:35 -0400

    Added swagger to project.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
